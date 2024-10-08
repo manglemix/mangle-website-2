e1f1dc1a698095d6bb3ace9faf541a38e949ced5
Najman Husaini <najmanhusaini20@gmail.com>
2024-10-07 23:49:51 -0600

    hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import wasmPack from 'vite-plugin-wasm-pack';
 
 export default defineConfig({
 	plugins: [enhancedImages(), sveltekit()]
