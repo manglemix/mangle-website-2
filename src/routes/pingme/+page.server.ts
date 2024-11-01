9f6784da559fc220beae8f6d8080f4255183978f
Najman Husaini <najmanhusaini20@gmail.com>
2024-11-01 14:13:13 -0600

    draft

--- src/routes/pingme/+page.server.ts
+++ src/routes/pingme/+page.server.ts
@@ -1,6 +1,15 @@
+import { createClient } from '@vercel/kv';
+import { PING_REST_API_URL, PING_REST_API_TOKEN } from '$env/static/private';
+
 export const actions = {
 	default: async (event) => {
-		event.fetch("http://127.0.0.1:30000", {
+        const pings = createClient({
+            url: PING_REST_API_URL,
+            token: PING_REST_API_TOKEN,
+        });
+
+        const addr = await pings.get("ping_addr") as string;
+		event.fetch(new URL(addr), {
             method: "POST",
             body: "pingmetest"
         });
--- src/routes/pingme/update/+server.ts
+++ src/routes/pingme/update/+server.ts
@@ -1,5 +1,13 @@
+import { createClient } from '@vercel/kv';
+import { PING_REST_API_URL, PING_REST_API_TOKEN } from '$env/static/private';
+
 export async function POST({ request }) {
+    const pings = createClient({
+        url: PING_REST_API_URL,
+        token: PING_REST_API_TOKEN,
+    });
+    
 	const addr = await request.text();
-    console.log(addr);
+    pings.set("ping_addr", addr);
 	return new Response(null, { status: 204 });
 }
