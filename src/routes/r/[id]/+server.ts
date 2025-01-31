89ef871c349010a7222e98749f21b209037b087f
Najman Husaini <najmanhusaini20@gmail.com>
2025-01-31 12:48:01 -0700

    corrected status code

--- src/routes/r/[id]/+server.ts
+++ src/routes/r/[id]/+server.ts
@@ -4,7 +4,7 @@ import { get } from '@vercel/edge-config';
 export const GET: RequestHandler = async ({ params }) => {
 	const redirects: Record<string, string> = await get('redirects') ?? {};
 	if (redirects[params.id!]) {
-		redirect(302, redirects[params.id!]);
+		redirect(307, redirects[params.id!]);
 	}
 
 	error(404, 'Not found');
