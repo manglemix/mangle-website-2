5e9f2cd93c94344e79ef9a78664de744d71e57c4
Najman Husaini <najmanhusaini20@gmail.com>
2025-01-31 12:39:24 -0700

    added redirects

---
+++ src/routes/r/[id]/+server.ts
@@ -0,0 +1,11 @@
+import { error, redirect, type RequestHandler } from '@sveltejs/kit';
+import { get } from '@vercel/edge-config';
+
+export const GET: RequestHandler = async ({ params }) => {
+	const redirects: Record<string, string> = await get('redirects') ?? {};
+	if (redirects[params.id!]) {
+		redirect(302, redirects[params.id!]);
+	}
+
+	error(404, 'Not found');
+};
