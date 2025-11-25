47ac4eeba656b15619b23122a89a14db571877f3
manglemix <shabouza030@gmail.com>
2025-11-25 12:27:53 -0700

    progress

---
+++ src/routes/blogs/maturing/+page.svelte
@@ -0,0 +1,72 @@
+<h1>
+    Maturing
+</h1>
+
+<p class="subtitle">
+    An autobiography of my life from 2021 to 2025
+</p>
+
+<p>
+    Growing into an adult is strange. While I will admit I had it easier than others my age, I would say I was still unprepared for what it would feel like. As of the time of writing, I freshly made the decision to not move forward with my OPT and leave the US permanently leaving barely 4 days after final exams week to relax. As will become evident, I have come to learn that change bothers me. More specifically, not having a home that I know will be where I sleep, eat, and play games, for a few years makes me anxious. It takes my brain time to acclimate and familiarize myself with the area I live in, so having to move always made me worried. However, it is not like I have never had a place to sleep. I know even if I get deported from the US I get to live in Malaysia. It's the lack of permanence that I had an issue with.
+</p>
+
+<p>
+    With the rapidly approaching graduation date I was once again faced with this fear. This was further compounded by the fact that I did not see it coming; Really, I did not. I will explain the mistake later on, but I had assumed I would have a few months of 2026 to enjoy in the US but that was not true. To help me come to terms with my departure, I decided to write out my entire university life since it was obviously a huge part of my life that is now over, and my brain is spinning in circles trying to reason over it all. I have been told I have good memory and recall, and that my life in the US was quite interesting, so I hope that you find some entertainment in this mental exercise I've put upon myself.
+</p>
+
+<p>
+    This next fact is not that important, but this is the <a href="https://youtu.be/w9I3GSQpO7M?si=KWjYk0B-wW65RKf-">album</a> that I am listening to as I write, if you wanted to get in the same headspace.
+</p>
+
+<h2>
+    2021
+</h2>
+
+<p class="subtitle">
+    Moving real fast
+</p>
+
+<p>
+    I was 17, and the worst of COVID had just barely passed. I was in Subang Jaya, Malaysia, at the time. Fortunately, we are one of the many countries that took COVID seriously and took countermeasures so our death toll was low. At the end of 2020 I finished my IGCSE which marked the end of my 12 year life at Sri Kuala Lumpur; 1 year in Preschool-2, 6 years in Primary school, and 5 years in Secondary school. Looking back, I didn't have the fear I do today of closing such large chapters since I was coming out of it with all my friends. It was a shared feeling, and I was promised by my parents to have at least half a year of relaxation. During the pandemic, I discovered the Godot game engine and I was enraptured by the idea of becoming a video game developer. I wanted all the time I had to explore ideas. On top of this, I was getting back into the swing of rock climbing and signed up for a climbing course at KL Eco City. This shopping mall was only one monorail ride away, and the station was walking distance from my house. It's funny &mdash; I felt like such an adult going to this course all by myself and coming back in the evening to work on my own projects.
+</p>
+
+<p>
+    My friends all followed roughly the same path and went to college, which outside of the US usually just means an introductory institution to university. The main classes are math and engineering, and maybe writing? I'm not sure since I never went. If you wanted to study in the UK or Australia, going to college was the usual route as many colleges had programs specifically for that to save you time and ease your transition. I wanted the notoriety of going to the US so that's why I skippd on college.
+</p>
+
+<p>
+    I enjoyed a few months of freedom and over-sleeping until my mom got me up one day and told me to go find a university to study at. That day we went to the very same college my friends were at and asked around. While they did offer a route to the US, it was to Ivy League schools which I did not want to do. I wanted the easy route. I still have mixed feelings about this decision today which I will discuss more later. Eventually I found myself in an online conference for finding American Institutions to study, but it was just me and one other person with several advisors. We both got a one-on-one session and I was able to describe my needs clearly; I wanted a school that taught Game Design, and I wanted it to be in a place with rock climbing. Instantly, the recruiter brought up the University of Utah. They were one of the few schools in the US that had a specific Game Design major and amongst their competitors they seemed to be the best, or at least the most serious. Also, if you are an avid climber you might know that Salt Lake City, Utah, is where the IFSC competitions are commonly held. This was the perfect intersection. The best part? The organization holding the conference had a program with the U of U to accept Malaysian students using their IGCSE results instead of having to take the SATs. All I needed to do was take the IELTs exam which was easy given that I am fluent in English and not my native language Malay.
+</p>
+
+<p>
+
+</p>
+
+<style>
+    h1 {
+        font-size: 3rem;
+        font-weight: bold;
+        margin: 0.5rem 0.5rem 0 0.5rem;
+    }
+    h2 {
+        font-size: 2.5rem;
+        font-weight: bold;
+        margin: 0.5rem 0.5rem 0 0.5rem;
+    }
+	* {
+		font-family: 'Times New Roman';
+	}
+    .subtitle {
+        text-indent: 0;
+        font-style: italic;
+    }
+    p {
+        text-indent: 2rem;
+        font-size: 1rem;
+        margin: 0.5rem 0.5rem 1rem 0.5rem;
+    }
+    a {
+        text-decoration: underline;
+        color: rgb(119, 119, 234);
+    }
+</style>
